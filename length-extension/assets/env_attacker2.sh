5e0d5758e7432aac55cfe0d27707b5960e421f7b
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-01-10 18:34:41 +0100

    move sha algo to step9

---
+++ length-extension/assets/env_attacker2.sh
@@ -0,0 +1,1 @@
+wget https://gitlab.itsec.ur.de/itsec/uebung/length-extension/raw/master/sha1.py
--- length-extension/assets/setup.sh
+++ length-extension/assets/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 apt-get install -y ascii
 
-wget https://gitlab.itsec.ur.de/itsec/uebung/length-extension/raw/master/sha1.py
-
 # solution
 curl -o .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/katacoda-scenarios/-/raw/main/solution.sh
 bash .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/length-extension/-/archive/master/length-extension-master.zip?path=katacoda-quiz-solution
