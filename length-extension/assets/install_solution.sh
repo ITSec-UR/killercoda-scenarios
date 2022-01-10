1ce401f3f92ad74c17c67c89821dd1068e3a0ec6
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-01-10 16:35:55 +0100

    fixed solution

---
+++ length-extension/assets/install_solution.sh
@@ -0,0 +1,2 @@
+curl -o .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/katacoda-scenarios/-/raw/main/solution.sh
+bash .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/length-extension/-/archive/master/length-extension-master.zip?path=katacoda-quiz-solution
