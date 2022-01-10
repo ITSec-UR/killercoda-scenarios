9a3d36d821c0b25344ba08612c61c3fee1c1a08d
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-01-10 17:39:02 +0100

    added progress bar

--- length-extension/assets/setup.sh
+++ length-extension/assets/setup.sh
@@ -2,3 +2,9 @@
 apt-get install -y ascii
 
 wget https://gitlab.itsec.ur.de/itsec/uebung/length-extension/raw/master/sha1.py
+
+# solution
+curl -o .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/katacoda-scenarios/-/raw/main/solution.sh
+bash .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/length-extension/-/archive/master/length-extension-master.zip?path=katacoda-quiz-solution
+
+echo "done" >> /root/.katacoda-finished
