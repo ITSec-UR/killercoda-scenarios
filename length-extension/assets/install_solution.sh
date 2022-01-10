668376852f05510b33b09f739b8a370ea4a6bde3
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-01-10 17:15:03 +0100

    background script

--- length-extension/assets/install_solution.sh
+++ length-extension/assets/install_solution.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/katacoda-scenarios/-/raw/main/solution.sh >/dev/null 2>&1
+curl -o .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/katacoda-scenarios/-/raw/main/solution.sh
 bash .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/length-extension/-/archive/master/length-extension-master.zip?path=katacoda-quiz-solution >/dev/null 2>&1
 echo "Webpage for solutions is configured and ready!"
