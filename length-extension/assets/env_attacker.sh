85edcf8735e9e74fd189cb7a8bcfbcd69b27bc63
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-01-10 19:56:51 +0100

    env_attacker multiple attempts

--- length-extension/assets/env_attacker.sh
+++ length-extension/assets/env_attacker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-k="topsecret"                       # secret symmetric key
-m1="bus=1"                          # original message
-mac1=$(echo -n $k$m1 | sha1sum)     # original mac h(k||m1)
+k="topsecret"                                       # secret symmetric key
+m1="bus=1"                                          # original message
+mac1=$(echo -n $k$m1 | sha1sum | awk '{print $1}')  # original mac h(k||m1)
 pad="8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000070"
 
 echo m1="$m1" >> /root/.environment
--- length-extension/assets/foreground.sh
+++ length-extension/assets/foreground.sh
@@ -1,2 +1,6 @@
-sleep 1
-progress.sh
+counter=0
+while [ "$counter" -lt 5] && [ $? -ne 127]
+do
+   progress.sh
+   sleep 1
+done
