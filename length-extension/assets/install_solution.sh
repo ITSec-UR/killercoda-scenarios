66f79d0d6634a38c85b953333ccc70e0c22af3d1
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-01-10 17:11:51 +0100

    suppress output foreground script

--- length-extension/assets/install_solution.sh
+++ length-extension/assets/install_solution.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl -o .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/katacoda-scenarios/-/raw/main/solution.sh
-bash .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/length-extension/-/archive/master/length-extension-master.zip?path=katacoda-quiz-solution
+curl -o .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/katacoda-scenarios/-/raw/main/solution.sh >/dev/null 2>&1
+bash .solution.sh https://gitlab.itsec.ur.de/itsec/uebung/length-extension/-/archive/master/length-extension-master.zip?path=katacoda-quiz-solution >/dev/null 2>&1
+echo "Webpage for solutions is configured and ready!"
