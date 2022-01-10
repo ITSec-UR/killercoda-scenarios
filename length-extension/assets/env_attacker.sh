07f1cd7c30a6e4142f2be33382b7dbd7f62d2f86
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-01-10 23:45:51 +0100

    added asoc array

--- length-extension/assets/env_attacker.sh
+++ length-extension/assets/env_attacker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-k="topsecret"                                       # secret symmetric key
-m1="bus=1"                                          # original message
-mac1=$(echo -n $k$m1 | sha1sum | awk '{print $1}')  # original mac h(k||m1)
-pad="8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000070"
+k="topsecret"                                                 # secret symmetric key
 
-echo 'export m1="'$m1'"' >> /root/.environment
-echo 'export mac1="'$mac1'"' >> /root/.environment
-echo 'export pad="'$pad'"' >> /root/.environment
+declare -A env
+env[m1]="bus=1"                                               # original message
+env[mac1]=$(echo -n $k$env[m1] | sha1sum | awk '{print $1}')  # original mac h(k||m1)
+env[pad]="8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000070"
+
+for i in "${!env[@]}"
+do
+   echo "export $i="'"'${env[$i]}'"' >> /root/.environment
+done
 
 echo 'source /root/.environment' >> /root/.bashrc
 
