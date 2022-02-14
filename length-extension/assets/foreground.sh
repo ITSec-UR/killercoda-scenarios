e51d4c829756a291d72a33a4c1586735941bd6d4
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-02-14 03:54:03 +0100

    Wait until solution-web is online

--- length-extension/assets/foreground.sh
+++ length-extension/assets/foreground.sh
@@ -1,17 +1,16 @@
 sleep 1
 counter=0
-timeout_time=90
 while [ "$counter" -lt 5 ] ; do
    ((counter+=1))
-   #progress.sh
-   timeout $timeout_time wait.sh
+   timeout 90 wait.sh
    exit_code=$?
    if [ $exit_code -ne 127 ] ; then break; else sleep 1 ; fi
    
 done
 source /root/.environment
+echo -e 'unset counter\nunset exit_code' >> .clean.sh
 echo clear >> .clean.sh
-echo 'if [ $exit_code -eq 124 ] ; then echo "Timeout ($timeout_time): Configuring" ; else echo Configured' >> .clean.sh
+echo 'if [ $exit_code -eq 124 ]; then echo "Timeout (90s): Configuring"; else echo Configured; fi' >> .clean.sh
 echo 'rm -- "$0"' >> .clean.sh
 cat /dev/null > ~/.bash_history && history -c
 bash .clean.sh
