d691b53a6a5e7071a62c6455f61c1d2edd8342ae
Maximilian Wittig <maximilian.wittig@stud.uni-regensburg.de>
2022-02-14 09:39:04 +0100

    Test

--- length-extension/assets/foreground.sh
+++ length-extension/assets/foreground.sh
@@ -4,13 +4,15 @@ while [ "$counter" -lt 5 ] ; do
    ((counter+=1))
    timeout 5 wait.sh
    exit_code=$?
-   if [ $exit_code -ne 127 ] ; then break; else sleep 1 ; fi
+   if [ "$exit_code" -eq 124 ] ; then is_timeout="true"; fi
+   if [ "$exit_code" -ne 127 ] ; then break; else sleep 1 ; fi
    
 done
 unset counter
+unset exit_code
 source /root/.environment
 echo clear >> .clean.sh
-echo 'if [[ "$exit_code" -eq 124 ]]; then echo "Timeout (90s): Configuring"; else echo Configured; fi' >> .clean.sh
+echo 'if [[ "$is_timeout" == "true" ]]; then echo "Timeout (90s): Configuring"; else echo Configured; fi' >> .clean.sh
 echo 'rm -- "$0"' >> .clean.sh
 cat /dev/null > ~/.bash_history && history -c
 bash .clean.sh
