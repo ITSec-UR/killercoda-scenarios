2d2313429270dcdc0611902dca50f03b97bec4a3
kag32683 <gina.kastner@stud.uni-regensburg.de>
2022-06-27 15:41:32 +0200

    update

--- xss-sh-csrf/assets/attacker-xss-cookie.sh
+++ xss-sh-csrf/assets/attacker-xss-cookie.sh
@@ -5,6 +5,10 @@ TASK_NAME="xss-highlight"
 DIRECTORY_NAME="web-attacker"
 
 ACTUAL_PATH="${INSTALL_PATH}/${TASK_NAME}/${TASK_NAME}-main/${DIRECTORY_NAME}/xss-cookie.php"
-SYMLINK_PATH="${DIRECTORY_NAME}/xss-cookie.php"
+SYMLINK_PATH="/${DIRECTORY_NAME}/xss-cookie.php"
+
+if [ ! -d "/${DIRECTORY_NAME}" ]; then
+  mkdir -p "/${DIRECTORY_NAME}"
+fi
 
 ln -s "${ACTUAL_PATH}" "${SYMLINK_PATH}"
