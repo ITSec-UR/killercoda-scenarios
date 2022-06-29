e05fef0df24bef6ec08bca273a71638820db58ae
kag32683 <gina.kastner@stud.uni-regensburg.de>
2022-06-29 15:32:11 +0200

    update attacker symlink path

--- xss/assets/attacker-xss-cookie.sh
+++ xss/assets/attacker-xss-cookie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ACTUAL_PATH=/opt/containerd/xss/*/web/$web-attacker/xss-cookie.php
+ACTUAL_PATH=/opt/containerd/xss/*/web/web-attacker/xss-cookie.php
 SYMLINK_PATH=/web-attacker/xss-cookie.php
 
 ln -s $ACTUAL_PATH $SYMLINK_PATH
