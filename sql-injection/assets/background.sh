8c9bb88dd6550857c8bb5399ff1ae49877dd8766
Maximilian Wittig <maximilian.wittig@informatik.uni-regensburg.de>
2022-06-29 13:42:14 +0000

    Update background.sh

--- sql-injection/assets/background.sh
+++ sql-injection/assets/background.sh
@@ -12,13 +12,13 @@ CONTAINER_NAME_DB="db"
 CONTAINER_NAME_SOLUTION="solution-sql-injection"
 
 if [ ! -d "$INSTALL_PATH" ]; then
-  mkdir -p $INSTALL_PATH
+  mkdir -p "$INSTALL_PATH"
 fi
 
 curl -o "${INSTALL_PATH}/sql-injection.zip" "$SOURCE"
-unzip -d ${NAME} "${INSTALL_PATH}/sql-injection.zip"
+unzip -d "${NAME}" "${INSTALL_PATH}/sql-injection.zip"
 rm -rf "${INSTALL_PATH}/sql-injection.zip"
-docker-compose -f ${NAME}/*/docker-compose.yml up -d
+docker-compose -f "${NAME}"/*/docker-compose.yml up -d
 
 # check if containers are running
 until [[  ("`docker inspect -f {{.State.Running}} $CONTAINER_NAME_WEB_1`" == "true") &&
@@ -30,7 +30,7 @@ until [[  ("`docker inspect -f {{.State.Running}} $CONTAINER_NAME_WEB_1`" == "tr
 done;
 
 # solution web
-docker-compose -f ${NAME}/*/quiz-solution/docker-compose.yml up -d
+docker-compose -f "${NAME}"/*/quiz-solution/docker-compose.yml up -d
 until [[ "`docker inspect -f {{.State.Running}} $CONTAINER_NAME_SOLUTION`" == "true" ]]; do
    sleep 0.1;
 done;
