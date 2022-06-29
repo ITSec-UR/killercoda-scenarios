aef71d5e2ab8767ca0bcfe3184bc70a4fa74cc40
kag32683 <gina.kastner@stud.uni-regensburg.de>
2022-06-29 15:26:52 +0200

    update attacker symlink path

--- xss/assets/attacker-xss-cookie.sh
+++ xss/assets/attacker-xss-cookie.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 INSTALL_PATH="/opt/containerd"
-TASK_NAME="xss"
-DIRECTORY_NAME="web-attacker"
+NAME="${INSTALL_PATH}/xss"
+DIRECTORY="web-attacker"
 
-ACTUAL_PATH="${INSTALL_PATH}/${TASK_NAME}/${TASK_NAME}-main/web/${DIRECTORY_NAME}/xss-cookie.php"
+ACTUAL_PATH="${NAME}/*/web/${DIRECTORY}/xss-cookie.php"
 SYMLINK_PATH="/${DIRECTORY_NAME}/xss-cookie.php"
 
-if [ ! -d "/${DIRECTORY_NAME}" ]; then
-  mkdir "${DIRECTORY_NAME}"
+if [ ! -d "/${DIRECTORY}" ]; then
+  mkdir "${DIRECTORY}"
 fi
 
-ln -s "${ACTUAL_PATH}" "${SYMLINK_PATH}"
+ln -s "$ACTUAL_PATH" "$SYMLINK_PATH"
