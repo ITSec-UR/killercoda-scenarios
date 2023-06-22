6efcb8809bb26a991f52d439860497a54176b50b
Maximilian Wittig <maximilian.wittig@informatik.uni-regensburg.de>
2023-06-22 13:38:39 +0000

    Fix

--- xss/assets/attacker-xss-cookie.sh
+++ xss/assets/attacker-xss-cookie.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ATTACKER_FOLDER=/opt/containerd/xss/*/web/web-attacker
-ATTACK_LINK="/root/web-attacker/"
+ATTACK_LINK=/root/web-attacker
 
 if [ ! -L ${ATTACK_LINK} ] ; then
     # Add sym link
     ln -s -T $ATTACKER_FOLDER $ATTACK_LINK
 fi
 
-echo "<?php\n// This website should save cookies which are sent via the URL parameters to a file" > "${ATTACK_LINK}/xss-cookie.php"
+echo -e "<?php\n// This website should save cookies which are sent via the URL parameters to a file" > "${ATTACK_LINK}/xss-cookie.php"
 rm -- "$0"
