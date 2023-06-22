d511ccc6daad2b7dd9c992b9fb6dd872fa35a548
Maximilian Wittig <maximilian.wittig@informatik.uni-regensburg.de>
2023-06-22 14:27:30 +0000

    Fix symlinks

--- xss/assets/attacker-csrf.sh
+++ xss/assets/attacker-csrf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ATTACKER_FOLDER=/opt/containerd/xss/*/web/web-attacker
-ATTACK_LINK="/root/web-attacker/"
+ATTACK_LINK=root/web-attacker
 
 if [ ! -L ${ATTACK_LINK} ] ; then
     # Add sym link
     ln -s -T $ATTACKER_FOLDER $ATTACK_LINK
 fi
 
-echo "<?php\n// <?php\n// This website should send a post request to SkyBalloon website in order to add a new highlight\n// by using the visitor's active and authenticated session (cross site request forgery)" > "${ATTACK_LINK}/csrf.php"
+echo -e "<?php\n// <?php\n// This website should send a post request to SkyBalloon website in order to add a new highlight\n// by using the visitor's active and authenticated session (cross site request forgery)" > "${ATTACK_LINK}/csrf.php"
 rm -- "$0"
--- xss/assets/attacker-xss-iframe.sh
+++ xss/assets/attacker-xss-iframe.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ATTACKER_FOLDER=/opt/containerd/xss/*/web/web-attacker
-ATTACK_LINK="/root/web-attacker/"
+ATTACK_LINK=/root/web-attacker
 
 if [ ! -L ${ATTACK_LINK} ] ; then
     # Add sym link
     ln -s -T $ATTACKER_FOLDER $ATTACK_LINK
 fi
 
-echo "<?php\n// This website should be embedded into the original website using XSS (iframe)\n// (1) Spoof original SkyBallon login window (copy html code, fix link issues)\n// (2) Phish login data and save the credentials to a file" > "${ATTACK_LINK}/fake-login.php"
-echo "<?php\n// This website has the attacking payload to embed an iframe of the fake-login.php into the original website\n// by exploiting the unsafe error message output in the SkyBalloon login window" > "${ATTACK_LINK}/xss-iframe.php"
+echo -e "<?php\n// This website should be embedded into the original website using XSS (iframe)\n// (1) Spoof original SkyBallon login window (copy html code, fix link issues)\n// (2) Phish login data and save the credentials to a file" > "${ATTACK_LINK}/fake-login.php"
+echo -e "<?php\n// This website has the attacking payload to embed an iframe of the fake-login.php into the original website\n// by exploiting the unsafe error message output in the SkyBalloon login window" > "${ATTACK_LINK}/xss-iframe.php"
 rm -- "$0"
