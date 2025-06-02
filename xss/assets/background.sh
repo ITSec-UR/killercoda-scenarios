e37ec84d6ebaf9b4f79204a0fc1ff4296b4c1be3
Maximilian Wittig <maximilian.wittig@informatik.uni-regensburg.de>
2025-06-02 17:59:41 +0200

    Rename domains in background

--- xss/assets/background.sh
+++ xss/assets/background.sh
@@ -11,9 +11,7 @@ CONTAINER_NAME_WEB_3="web-attacker"
 CONTAINER_NAME_DB="db-xss"
 CONTAINER_NAME_SOLUTION="solution-xss"
 
-if [ ! -d "$INSTALL_PATH" ]; then
-  mkdir -p "$INSTALL_PATH"
-fi
+mkdir -p "$INSTALL_PATH"
 
 curl -o "${INSTALL_PATH}/xss.zip" "$SOURCE"
 unzip -d "${NAME}" "${INSTALL_PATH}/xss.zip"
@@ -37,8 +35,18 @@ docker exec web-skyballoon chown www-data:www-data /tmp/sess_75639f6c2bcf4f8a4e7
 docker exec web-skyballoon chmod 777 /tmp
 docker exec web-attacker chown www-data:www-data /var/www/html
 
+# rename domains in solution to current killercoda scenario
+skyballoon_port=$(docker port "$CONTAINER_NAME_WEB_2" 80/tcp | awk -F: '{print $2}' | head -n1)
+attacker_port=$(docker port "$CONTAINER_NAME_WEB_3" 80/tcp | awk -F: '{print $2}' | head -n1)
+skyballoon_domain=$(sed "s/PORT/${skyballoon_port}/g" /etc/killercoda/host)
+attacker_domain=$(sed "s/PORT/${attacker_port}/g" /etc/killercoda/host)
+target_file="$NAME/xss-main/quiz-solution/html/quiz.json"
+sed -i "s|<skyBalloon_domain>|$skyballoon_domain|g" "$target_file"
+sed -i "s|<attacker_domain>|$attacker_domain|g" "$target_file"
+
 # solution web
 docker-compose -f "${NAME}"/*/quiz-solution/docker-compose.yml up -d
+
 until [[ "`docker inspect -f {{.State.Running}} $CONTAINER_NAME_SOLUTION`" == "true" ]]; do
    sleep 0.1;
 done;
